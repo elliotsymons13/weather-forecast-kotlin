c82d3637eab649a715dccc3c8a1a5fc001e9d7af
Elliot S <elliotsymons13@gmail.com>
2025-04-09 16:29:06 +0100

    ADD contact test infrastructure

--- src/test/kotlin/com/oocode/MainKtTest.kt
+++ src/test/kotlin/com/oocode/MainKtTest.kt
@@ -6,6 +6,7 @@ import com.natpryce.hamkrest.equalTo
 import com.teamoptimization.AcmeForecasterClient
 import moo
 import org.http4k.client.JavaHttpClient
+import org.http4k.core.HttpHandler
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -14,6 +15,43 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertContains
 import kotlin.test.assertNotNull
 
+abstract class AcmeDataContract {
+    abstract val httpClient: HttpHandler
+
+    @Test
+    fun `can parse known response`() {
+
+        val forecastClient = AcmeForecasterClient(httpClient)
+        val forecastData = forecastClient.acmeForecast("Monday", "Oxford")
+
+        assertNotNull(forecastData.min)
+        assertNotNull(forecastData.max)
+        assertNotNull(forecastData.description)
+
+    }
+}
+
+class AcmeDataFake : AcmeDataContract() {
+    private val hardCodedJsonResponse = "{\"min\": 5, \"max\": 12, \"description\": \"Cold and rainy\"}"
+    override val httpClient: HttpHandler  = { Response(Status.OK).body(hardCodedJsonResponse) }
+
+    @Test
+    fun `can parse known responsex`() {
+
+        val forecastClient = AcmeForecasterClient(httpClient)
+        val forecastData = forecastClient.acmeForecast("Monday", "Oxford")
+
+        assertEquals(forecastData.max, "12")
+        assertEquals(forecastData.min, "5")
+        assertEquals(forecastData.description, "Cold and rainy")
+
+    }
+}
+
+class IntegrationTest : AcmeDataContract() {
+    override val httpClient: HttpHandler = JavaHttpClient()
+}
+
 internal class MainKtTest {
     @Disabled
     @Test
@@ -21,17 +59,5 @@ internal class MainKtTest {
         assertThat(moo(), equalTo("boo"))
     }
 
-    @Test
-    fun `can parse known response`() {
-//        val httpClient = JavaHttpClient()
-//        val forecastClient = AcmeForecasterClient(httpClient)
 
-        val hardCodedJsonResponse = "{\"min\": 5, \"max\": 12, \"description\": \"Cold and rainy\"}"
-        val forecastClient = AcmeForecasterClient({ Response(Status.OK).body(hardCodedJsonResponse) })
-        val forecastData = forecastClient.acmeForecast("Monday", "Oxford")
-
-        assertEquals(forecastData.max, "12")
-        assertEquals(forecastData.min, "5")
-        assertEquals(forecastData.description, "Cold and rainy")
-    }
 }
